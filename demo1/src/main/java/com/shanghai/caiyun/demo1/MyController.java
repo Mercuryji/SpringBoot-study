bef40d2cb7dc6247c6423a64be238be250931d8a
Mercuryji <mercuryji@126.com>
2018-03-12 20:02:34 +0800

    demo1

--- demo1/src/main/java/com/shanghai/caiyun/demo1/MyController.java
+++ demo1/src/main/java/com/shanghai/caiyun/demo1/MyController.java
@@ -1,12 +1,17 @@
 package com.shanghai.caiyun.demo1;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/index")
 public class MyController {
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @PostMapping("/addPerson")
+    public Person addPerson(@RequestBody Person p1){
+        return p1;
     }
 }
---
+++ demo1/src/main/java/com/shanghai/caiyun/demo1/Person.java
@@ -0,0 +1,33 @@
+package com.shanghai.caiyun.demo1;
+
+
+
+public class Person {
+    private int id;
+    private String name;
+    private String gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
